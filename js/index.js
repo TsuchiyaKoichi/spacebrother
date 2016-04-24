7da7fb25c04d0db80cd518030e765b9b10d5c234
TsuchiyaKoichi <lival1116@gmail.com>
2016-04-24 15:02:40 +0900

    add animations

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 $(function() {
+  
+  var $tiles = $('.tile');
+  
   $(window).on('scroll', function() {
     var y = $(this).scrollTop();
-    console.log(y);
+    
+    $tiles.each(function(index) {
+      if($(this).position().top - 400 < y) {
+        $(this).addClass('tile' + (index + 1) + '-anime');
+      }
+    });
   });
 });
